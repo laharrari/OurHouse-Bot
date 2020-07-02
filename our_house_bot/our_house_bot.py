8874d7978fae3c0bb1dfdbf9e11c7609e67da93c
Harrison Lee <laharrari@gmail.com>
2020-07-02 04:21:10 -0700

    Changed Discord token grabbing for Heroku token authentication.

--- our_house_bot/our_house_bot.py
+++ our_house_bot/our_house_bot.py
@@ -1,4 +1,4 @@
-from our_house_bot.common_utils import discord_token
+import os
 from discord.ext import commands
 from discord.ext.commands import DefaultHelpCommand
 
@@ -6,13 +6,11 @@ class OurHouse(commands.Bot):
     def __init__(self, **options):
         super().__init__(".", help_command = None, **options)
 
-        self.discord_token = discord_token
-
         from our_house_bot.cogs.queue_cog import QueueCog
         self.add_cog(QueueCog(self))
 
     def run(self, *args, **kwargs):
-        super().run(self.discord_token, *args, **kwargs)
+        super().run(os.environ["DISCORD_TOKEN"], *args, **kwargs)
 
     async def on_ready(self):
         print("Bot is ready!")
